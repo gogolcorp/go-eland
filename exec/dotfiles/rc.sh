6990af0408afdf7cd83c056a74a180ae60c1fd7c
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-11-15 12:01:41 +0100

    upd(dotfiles): #3 - new logic + new ui + exec folder structure

---
+++ exec/dotfiles/rc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source "$PWD"/config/ui.sh
+source "$PWD"/config/cfg.sh
+
+ui_start
+
+sourcing="source $HOME/.aliases; source $HOME/.functions"
+
+
+if [ -f "$HOME"/.zshrc ]; then
+  ui_info "$HOME/.zshrc detected"
+  if ! grep -q "$sourcing" < "$HOME"/.zshrc ; then
+    ui_info "dotfile sourcing not found"
+    ui_cmd "printf 'source $HOME/.aliases; source $HOME/.functions' >> $HOME/.zshrc"
+    printf "\n%s\n" "$sourcing" >> "$HOME"/.zshrc
+  else
+    ui_info "sourcing already found, terminating" 
+  fi
+else
+  ui_error "$HOME/.zshrc not found"
+fi
+
+ui_done
