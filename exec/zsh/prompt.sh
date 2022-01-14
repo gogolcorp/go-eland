a78b4e32cec8e5087e3d03350ca6d744c4592e37
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-01-14 11:47:07 +0100

    add(zsh): #5 - spaceship prompt install

---
+++ exec/zsh/prompt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source "$PWD"/config/ui.sh
+source "$PWD"/config/cfg.sh
+
+exec_a=(git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.oh-my-zsh/themes/spaceship-prompt" --depth=1)
+exec_b=(ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme")
+exec_c=(sed -i.old "s/^ZSH_THEME=\".*/ZSH_THEME=\"spaceship\"/g" "$HOME"/.zshrc)
+
+ui_start
+
+ui_info "installing 'spaceship-prompt' theme"
+ui_cmd "${exec_a[@]}" ; "${exec_a[@]}"
+
+ui_info "create symlink"
+ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
+
+ui_info "update .zshrc with current prompt"
+ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
+
+ui_done
