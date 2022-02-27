14d9b320cc854432f7b8623191e58a8cc5810aed
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-02-27 16:36:24 +0100

    fix(docker): #16 - docker install exec code

--- exec/clis/docker.sh
+++ exec/clis/docker.sh
@@ -4,17 +4,17 @@
 source "$PWD"/config/ui.sh
 source "$PWD"/config/cfg.sh
 
-exec_a=$(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -)
+exec_a="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
 exec_b=(sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable")
 exec_c=(sudo apt install docker-ce)
 exec_d=(sudo systemctl status docker)
 exec_e=(sudo usermod -aG docker "${USER}")
-exec_f=(su - "${USER}")
+exec_f="su - ${USER}"
 
 ui_start
 
 ui_info "Add the GPG key for the official Docker repository"
-ui_cmd "${exec_a[@]}" ; curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ui_cmd "$exec_a" ; $exec_a
 
 ui_info "Add the Docker repository to APT sources"
 ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
@@ -29,6 +29,6 @@ ui_info "Add your username to the docker group"
 ui_cmd "${exec_e[@]}" ; "${exec_e[@]}"
 
 ui_info "Refresh groups"
-ui_cmd "${exec_f[@]}" ; "${exec_f[@]}"
+ui_cmd "$exec_f" ; $exec_f
 
 ui_done
