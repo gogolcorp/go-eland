a444c13287b7f635f33b002d24730a37df8d412e
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-02-28 14:47:27 +0100

    fix(k8s): #7 - install exec code

--- exec/clis/k8s/helm.sh
+++ exec/clis/k8s/helm.sh
@@ -6,7 +6,7 @@ source "$PWD"/config/cfg.sh
 
 exec_a="curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -"
 exec_b="echo 'deb https://baltocdn.com/helm/stable/debian/ all main' | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list"
-exec_c=(sudo apt install helm -y)
+exec_c="sudo apt update -y; sudo apt install helm -y"
 
 ui_start
 
@@ -17,6 +17,6 @@ ui_info "Add the Helm apt repository"
 ui_cmd "$exec_b" ; echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
 ui_info "Install Helm"
-ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
+ui_cmd "$exec_c" ; sudo apt update -y; sudo apt install kubectl -y
 
 ui_done
--- exec/clis/k8s/kubectl.sh
+++ exec/clis/k8s/kubectl.sh
@@ -6,7 +6,7 @@ source "$PWD"/config/cfg.sh
 
 exec_a="sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg"
 exec_b="echo 'deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list"
-exec_c=(sudo apt install kubectl -y)
+exec_c="sudo apt update -y; sudo apt install kubectl -y"
 
 ui_start
 
@@ -17,6 +17,6 @@ ui_info "Add the Kubernetes apt repository"
 ui_cmd "$exec_b" ; echo 'deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 ui_info "Install Kubectl"
-ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
+ui_cmd "$exec_c" ; sudo apt update -y; sudo apt install kubectl -y
 
 ui_done
