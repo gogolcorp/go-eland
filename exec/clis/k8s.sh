5ac5094365d48d5821c8d7fa79096d320f883dca
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-03-03 11:27:02 +0100

    add(brew): #22 - new install type

--- exec/clis/k8s.sh
+++ exec/clis/k8s.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 # shellcheck source=/dev/null
-source "$PWD"/exec/clis/k8s/kubectl.sh
-source "$PWD"/exec/clis/k8s/helm.sh
+source "$PWD"/config/ui.sh
+source "$PWD"/config/cfg.sh
+
+
+ui_start
+
+for i in "${_brew_k8s_packages_[@]}"
+do
+  exec=(brew install $i -y)
+
+  ui_info "installing $i package"
+  ui_cmd "${exec[@]}" ; "${exec[@]}"
+done
+
+
+ui_done
