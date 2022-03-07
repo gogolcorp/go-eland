0548abac17c2e40c4d176933d0219ff90e5b0807
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-03-07 17:12:58 +0100

    upd(core): #22 - apply utils

--- exec/zsh/prompt.sh
+++ exec/zsh/prompt.sh
@@ -10,13 +10,18 @@ exec_c=(sed -i.old "s/^ZSH_THEME=\".*/ZSH_THEME=\"spaceship\"/g" "$HOME"/.zshrc)
 
 ui_start
 
-ui_info "installing 'spaceship-prompt' theme"
-ui_cmd "${exec_a[@]}" ; "${exec_a[@]}"
+if [ -d "$HOME"/.oh-my-zsh/custom/plugins/"$i" ]; then
+  ui_info "$i folder exists; skipping plugin"
+else
+  ui_info "installing 'spaceship-prompt' theme"
+  ui_cmd "${exec_a[@]}" ; "${exec_a[@]}"
 
-ui_info "create symlink"
-ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
+  ui_info "create symlink"
+  ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
 
-ui_info "update .zshrc with current prompt"
-ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
+  ui_info "update .zshrc with current prompt"
+  ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
+fi
+
 
 ui_done
