afb263be59a630ab98f2505871cb54076a2a9cbe
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-03-07 11:06:12 +0100

    fix(auto): #24 - fix packages

--- exec/packages/brew.sh
+++ exec/packages/brew.sh
@@ -9,7 +9,6 @@ exec_b=(brew update)
 exec_c=(brew upgrade)
 exec_d=(brew cleanup)
 exec_e=(brew doctor)
-exec_f="eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 ui_start
 
@@ -18,10 +17,10 @@ if [ "$(which brew)" != 0 ]; then
   ui_cmd "$exec_a" ; $exec_a
   if [ -f "$HOME"/.zshrc ]; then
     ui_info "$HOME/.zshrc detected"
-    if ! grep -q "$exec_f" < "$HOME"/.zshrc ; then
+    if ! grep -q "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" < "$HOME"/.zshrc ; then
       ui_info "dotfile sourcing not found"
-      ui_cmd "$exec_f >> $HOME/.zshrc"
-      printf "\n%s\n" "$exec_f" >> "$HOME"/.zshrc
+      ui_cmd "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) >> $HOME/.zshrc"
+      printf "\n%s\n" "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> "$HOME"/.zshrc
     else
       ui_info "sourcing already found, terminating" 
     fi
@@ -45,6 +44,6 @@ ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
 ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
 ui_cmd "${exec_d[@]}" ; "${exec_d[@]}"
 ui_cmd "${exec_e[@]}" ; "${exec_e[@]}"
-ui_cmd "${exec_f[@]}" ; "${exec_f[@]}"
+ui_cmd "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" ; "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 ui_done
