f63b49867034f59685a7fd0ccf7bfe561b6be6d3
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-03-07 15:08:09 +0100

    fix(auto): #24 - fix packages

--- exec/packages/brew.sh
+++ exec/packages/brew.sh
@@ -33,14 +33,6 @@ else
 fi
 
 
-for i in "${_brew_packages_[@]}"
-do
-  exec=(brew install "$i")
-
-  ui_info "installing $i package"
-  ui_cmd "${exec[@]}" ; "${exec[@]}"
-done
-
 ui_cmd "$(eval /home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "$(eval /home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
@@ -48,5 +40,13 @@ ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
 ui_cmd "${exec_c[@]}" ; "${exec_c[@]}"
 ui_cmd "${exec_d[@]}" ; "${exec_d[@]}"
 ui_cmd "${exec_e[@]}" ; "${exec_e[@]}"
+
+for i in "${_brew_packages_[@]}"
+do
+  exec=(brew install "$i")
+
+  ui_info "installing $i package"
+  ui_cmd "${exec[@]}" ; "${exec[@]}"
+done
 
 ui_done
