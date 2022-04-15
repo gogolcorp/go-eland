c19f4f63e6ab2105c6b6927a3af9ef262593e876
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-04-15 14:13:26 +0200

    upd(aliases): add more aliases + functions

--- config/files/functions.sh
+++ config/files/functions.sh
@@ -2,25 +2,21 @@
 
 # delete all Docker ressources
 function _docker_purge_() {
-  containers=$(docker ps -a -q)
-  images=$(docker images -q)
-  TICK="sleep 1 ; clear"
-
-  $TICK; echo -e "\e[33m[#] ANNIHILATION IN 3 [#]\e[0m"
-  $TICK; echo -e "\e[33m[#] ANNIHILATION IN 2 [#]\e[0m"
-  $TICK; echo -e "\e[31m[#] ANNIHILATION IN 1 [#]\e[0m"
-  $TICK; echo -e "\e[1;33;41m[!] I G N I T I O N [!]\e[0m"
-  $TICK; echo -e "\e[34m[1] STOPING CONTAINERS [1]\e[0m"
-  docker stop "$containers"
-  $TICK; echo -e "\e[34m[2] REMOVING CONTAINERS [2]\e[0m"
-  docker rm "$containers"
-  $TICK; echo -e "\e[34m[3] REMOVING IMAGES [3]\e[0m"
-  docker rmi "$images"
-  $TICK; echo -e "\e[34m[4] REMOVING VOLUMES [4]\e[0m"
+  sleep 1; clear; echo -e "\e[33m[#] ANNIHILATION IN 3 [#]\e[0m"
+  sleep 1; clear; echo -e "\e[33m[#] ANNIHILATION IN 2 [#]\e[0m"
+  sleep 1; clear; echo -e "\e[31m[#] ANNIHILATION IN 1 [#]\e[0m"
+  sleep 1; clear; echo -e "\e[1;33;41m[!] I G N I T I O N [!]\e[0m"
+  sleep 1; clear; echo -e "\e[34m[1] STOPING CONTAINERS [1]\e[0m"
+  docker stop $(docker ps -a -q)
+  sleep 1; clear; echo -e "\e[34m[2] REMOVING CONTAINERS [2]\e[0m"
+  docker rm $(docker ps -a -q)
+  sleep 1; clear; echo -e "\e[34m[3] REMOVING IMAGES [3]\e[0m"
+  docker rmi $(docker images -q)
+  sleep 1; clear; echo -e "\e[34m[4] REMOVING VOLUMES [4]\e[0m"
   docker system prune --all --volumes --force
-  $TICK; echo -e "\e[32m[5] SHOW OUTPUT [5]\e[0m"
+  sleep 1; clear; echo -e "\e[32m[5] SHOW OUTPUT [5]\e[0m"
   docker ps -a ; docker images
-  $TICK; echo -e "\e[1;33;42m[<] ANNIHILATION DONE [>]\e[0m"
+  sleep 1; clear; echo -e "\e[1;33;42m[<] ANNIHILATION DONE [>]\e[0m"
   return
 }
 
