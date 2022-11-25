f267d5ce6723ff04766eff52f11a04243de93034
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-11-25 14:53:10 +0100

    fi(logs): #33 - printf fix

--- exec/dotfiles/dotfiles.sh
+++ exec/dotfiles/dotfiles.sh
@@ -14,9 +14,14 @@ do
   exec_b=(cp "$PWD"/config/files/"$i".sh "$HOME"/."$i")
   if [ -f "$file" ]; then
     core__ui_info "$file already exists, creating old version"
-    core__ui_cmd "${exec_a[@]}" ; "${exec_a[@]}"
+
+    core__ui_cmd "${exec_a[@]}"; "${exec_a[@]}"
+    exit_code=$?
+    core__log_cmd $exit_code "${exec_a[*]}"
   fi
-  core__ui_cmd "${exec_b[@]}" ; "${exec_b[@]}"
+  core__ui_cmd "${exec_b[@]}"; "${exec_b[@]}"
+  exit_code=$?
+  core__log_cmd $exit_code "${exec_b[*]}"
 done
 
 core__ui_done
--- exec/dotfiles/rcfiles.sh
+++ exec/dotfiles/rcfiles.sh
@@ -7,15 +7,18 @@ source "$PWD"/config/utils.sh
 
 core__ui_start
 
-sourcing="source $HOME/.aliases; source $HOME/.functions"
+exec="printf source $HOME/.aliases; source $HOME/.functions >> $HOME/.zshrc "
 
 
 if [ -f "$HOME"/.zshrc ]; then
   core__ui_info "$HOME/.zshrc detected"
-  if ! grep -q "$sourcing" < "$HOME"/.zshrc ; then
+  if ! grep -q "$exec" < "$HOME"/.zshrc ; then
     core__ui_info "dotfile sourcing not found"
-    core__ui_cmd "$sourcing >> $HOME/.zshrc"
-    printf "\n%s\n" "$sourcing" >> "$HOME"/.zshrc
+    core__ui_cmd "printf \"$exec\" >> $HOME/.zshrc" ; printf "\n%s\n" "$exec" >> "$HOME"/.zshrc
+
+    exit_code=$?
+    core__log_cmd $exit_code "printf \"$exec\" >> $HOME/.zshrc"
+    # printf "\n%s\n" "$exec" >> "$HOME"/.zshrc
   else
     core__ui_info "sourcing already found, terminating" 
   fi
